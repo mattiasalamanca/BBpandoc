e3366072d5ca25b24c9623921a96c730cf919923
Mattia Salamanca Orrego <5004428+mattiasalamanca@users.noreply.github.com>
2018-12-28 13:37:16 +0100

    Update pandoc-html.sh
    Reflects required pandoc syntax changes.
    --smart/-S has been removed.  Use +smart or -smart extension instead.
    For example: pandoc -f markdown+smart -t markdown-smart.

--- pandoc-html.sh
+++ pandoc-html.sh
@@ -2,5 +2,4 @@
 PATH=$PATH:/usr/local/bin
 
 cd "$(dirname "$BB_DOC_PATH")"
-pandoc "${BB_DOC_PATH}" -o "${BB_DOC_PATH%%.*}".html -s --smart --mathjax --email-obfuscation=references
-
+pandoc "${BB_DOC_PATH}" -o "${BB_DOC_PATH%%.*}".html -s -f markdown+smart --mathjax --email-obfuscation=references --metadata pagetitle="${BB_DOC_PATH%%.*}"
